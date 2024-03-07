a944e3d1861fe14ea9bd80ff556996a4648b0325
Felipe Borba <felipe.borba0109@gmail.com>
2024-03-06 23:30:57 -0300

    add main list

--- app/src/main/java/com/example/youtuberemake/MainActivity.kt
+++ app/src/main/java/com/example/youtuberemake/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.youtuberemake
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.youtuberemake.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvMain.layoutManager = LinearLayoutManager(this)
+        binding.rvMain.adapter = VideoAdapter()
     }
 }
